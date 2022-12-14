ce2a075379f9d176d58709e96418f1a22263c5fa
Volodymyr Gamula <v.gamula@gmail.com>
2022-12-14 01:35:06 +0100

    2022-13 sign function is actually redundant

--- 2022/13/main.rkt
+++ 2022/13/main.rkt
@@ -3,15 +3,9 @@
 (require json)
 (require racket/file)
 
-(define (sign x)
-  (cond
-    [(< x 0) -1]
-    [(= x 0) 0]
-    [(> x 0) 1]))
-
 (define (comparator a b)
   (cond
-    [(and (number? a) (number? b)) (sign (- a b))]
+    [(and (number? a) (number? b)) (- a b)]
     [(and (number? a) (list? b)) (comparator (list a) b)]
     [(and (list? a) (number? b)) (comparator a (list b))]
     [(and (list? a) (list? b))
@@ -26,8 +20,8 @@
                     (take b min-len))])
        (if (not (= compare-list-res 0))
            compare-list-res
-           (sign (- (length a)
-                    (length b)))))]))
+           (- (length a)
+              (length b))))]))
 
 (define (pairs-in-order-weight packet-pairs)
   (let ([good-cmp-results-idxs (map (lambda (pair idx)
@@ -48,7 +42,8 @@
          [all-packets (append all-input-packets (list packet2 packet6))]
          [all-packets-sorted
           (sort all-packets (lambda (a b)
-                              (= -1 (comparator a b))))])
+                              (< (comparator a b)
+                                 0)))])
     (* (+ 1 (index-of all-packets-sorted packet2))
        (+ 1 (index-of all-packets-sorted packet6)))))
 
