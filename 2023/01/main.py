52a90a865ee3eb3f2eaa50a8d5d71a61009587c1
Volodymyr Gamula <v.gamula@gmail.com>
2023-12-01 16:23:12 +0000

    2023-01 for loop -> seq comprehension

--- 2023/01/main.py
+++ 2023/01/main.py
@@ -4,20 +4,22 @@ import fileinput
 lines = [line.strip() for line in f.readlines()]
 
 search_list1 = [*'123456789']
-search_list2 = search_list1 + 'one,two,three,four,five,six,seven,eight,nine'.split(',')
+search_list2 = search_list1 + 'one,two,three,four,five,six,seven,eight,nine'.split(
+    ',')
 word_to_num = {w: str(i + 1) for i, w in enumerate(search_list2[9:])}
+
 
 def find_index(line, search_list, find_fn, decider_fn):
   res = [(find_fn(line, s), s) for s in search_list if s in line]
   return word_to_num.get(r := decider_fn(res)[1], r)
 
+
 def task(search_list):
-  calibration = 0
-  for line in lines:
-    calibration += int(
-      find_index(line, search_list, str.index, min) +
-      find_index(line, search_list, str.rindex, max))
-  return calibration
+  return sum(
+      int(
+          find_index(line, search_list, str.index, min) +
+          find_index(line, search_list, str.rindex, max)) for line in lines)
+
 
 print(f'Task 1: {task(search_list1)}')
 print(f'Task 2: {task(search_list2)}')
