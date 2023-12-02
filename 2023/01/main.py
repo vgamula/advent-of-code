b0a15af809df7ecc6eecc16dd62081b30c1be084
Volodymyr Gamula <v.gamula@gmail.com>
2023-12-02 11:37:07 +0000

    Fix 2023-01 after copying from replit

--- 2023/01/main.py
+++ 2023/01/main.py
@@ -1,25 +1,27 @@
 import fileinput
 
 
-lines = [line.strip() for line in f.readlines()]
+lines = [line.strip() for line in fileinput.input()]
 
-search_list1 = [*'123456789']
-search_list2 = search_list1 + 'one,two,three,four,five,six,seven,eight,nine'.split(
-    ',')
+search_list1 = [*"123456789"]
+search_list2 = search_list1 + "one,two,three,four,five,six,seven,eight,nine".split(",")
 word_to_num = {w: str(i + 1) for i, w in enumerate(search_list2[9:])}
 
 
 def find_index(line, search_list, find_fn, decider_fn):
-  res = [(find_fn(line, s), s) for s in search_list if s in line]
-  return word_to_num.get(r := decider_fn(res)[1], r)
+    res = [(find_fn(line, s), s) for s in search_list if s in line]
+    return word_to_num.get(r := decider_fn(res)[1], r)
 
 
 def task(search_list):
-  return sum(
-      int(
-          find_index(line, search_list, str.index, min) +
-          find_index(line, search_list, str.rindex, max)) for line in lines)
+    return sum(
+        int(
+            find_index(line, search_list, str.index, min)
+            + find_index(line, search_list, str.rindex, max)
+        )
+        for line in lines
+    )
 
 
-print(f'Task 1: {task(search_list1)}')
-print(f'Task 2: {task(search_list2)}')
+print(f"Task 1: {task(search_list1)}")
+print(f"Task 2: {task(search_list2)}")
