ab96feaa0d07583b68e6e473030da29e56ec0d28
Volodymyr Gamula <v.gamula@gmail.com>
2024-12-02 20:20:13 +0000

    2024-02 Simplify solution

--- 2024/bin/day02.ml
+++ 2024/bin/day02.ml
@@ -6,15 +6,10 @@ let data =
   |> List.map ~f:(fun s -> String.split s ~on:' ' |> List.map ~f:Int.of_string)
 ;;
 
-let is_safe_report l =
-  let is_safe l =
-    List.for_all l ~f:(fun x -> 1 <= abs x && abs x <= 3)
-    && (List.for_all l ~f:(fun x -> x < 0) || List.for_all l ~f:(fun x -> 0 < x))
-  in
-  let calculate_diffs l =
+let is_floor_report_safe l =
+  let calculate_floor_diffs l =
     let rec inner previous l =
       match l with
-      | [ current ] -> [ previous - current ]
       | current :: next -> (previous - current) :: inner current next
       | _ -> []
     in
@@ -22,22 +17,26 @@ let is_safe_report l =
     | current :: next -> inner current next
     | _ -> []
   in
-  calculate_diffs l |> is_safe
+  let are_floor_diffs_acceptable l =
+    List.for_all l ~f:(fun x -> 1 <= abs x && abs x <= 3)
+    && (List.for_all l ~f:(( < ) 0) || List.for_all l ~f:(( > ) 0))
+  in
+  let diffs = calculate_floor_diffs l in
+  are_floor_diffs_acceptable diffs
 ;;
 
-let task1 = data |> List.filter ~f:is_safe_report |> List.length;;
+let task1 = data |> List.count ~f:is_floor_report_safe;;
 
 Stdio.printf "Task 1: %d\n" task1
 
 let task2 =
-  let generate_sublists l =
+  let generate_subreports l =
     List.init (List.length l) ~f:(( + ) 1)
     |> List.map ~f:(fun i -> List.filteri l ~f:(fun j _ -> i <> j + 1))
   in
   data
-  |> List.map ~f:generate_sublists
-  |> List.filter ~f:(List.exists ~f:is_safe_report)
-  |> List.length
+  |> List.map ~f:generate_subreports
+  |> List.count ~f:(List.exists ~f:is_floor_report_safe)
 ;;
 
 Stdio.printf "Task 2: %d\n" task2
