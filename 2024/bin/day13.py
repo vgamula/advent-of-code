a4d8b7a3fcc4090e15120f7ae58da7a6002b44b0
Volodymyr Gamula <v.gamula@gmail.com>
2024-12-14 00:06:27 +0100

    2024-13 Add pure math solution using Cramer's rule, simplify regex

--- 2024/bin/day13.py
+++ 2024/bin/day13.py
@@ -1,33 +1,56 @@
 import fileinput
 import re
-from sympy import symbols, Eq, solve
-
 
 lines = [line.strip() for line in fileinput.input() if line.strip()]
+
+
+def extract_x_y(s):
+    return list(map(int, re.findall(r"(\d+)", s)))
+
 
 machines = []
 for ii in range(len(lines) // 3):
     i = ii * 3
     machines.append(
         {
-            "Button1": list(map(int, re.findall(r"X\+(\d+), Y\+(\d+)", lines[i])[0])),
-            "Button2": list(map(int, re.findall(r"X\+(\d+), Y\+(\d+)", lines[i + 1])[0])),
-            "Prize": list(map(int, re.findall(r"X=(\d+), Y=(\d+)", lines[i + 2])[0])),
+            "Button1": extract_x_y(lines[i]),
+            "Button2": extract_x_y(lines[i + 1]),
+            "Prize": extract_x_y(lines[i + 2]),
         }
     )
 
 
-def task1(not_more_than_100=True, delta=0):
+def solve_using_sympy(not_more_than_100=True, delta=0):
+    from sympy import symbols, Eq, solve
+
     result = 0
-    for machine in machines:
+    for m in machines:
         a, b = symbols("a b", integer=True)
-        eq1 = Eq(machine["Button1"][0] * a + machine["Button2"][0] * b, machine["Prize"][0] + delta)
-        eq2 = Eq(machine["Button1"][1] * a + machine["Button2"][1] * b, machine["Prize"][1] + delta)
+        eq1 = Eq(m["Button1"][0] * a + m["Button2"][0] * b, m["Prize"][0] + delta)
+        eq2 = Eq(m["Button1"][1] * a + m["Button2"][1] * b, m["Prize"][1] + delta)
         solution = solve((eq1, eq2), (a, b))
-        if solution and (not not_more_than_100 or (not_more_than_100 and solution[a] <= 100 and solution[b] <= 100)):
+        if solution and (not not_more_than_100 or (solution[a] <= 100 and solution[b] <= 100)):
             result += 3 * solution[a] + solution[b]
     return result
 
 
-print("Task 1:", task1(not_more_than_100=True, delta=0))
-print("Task 2:", task1(not_more_than_100=False, delta=10000000000000))
+def solve_using_cramers_rule(not_more_than_100=True, delta=0):
+    result = 0
+    for m in machines:
+        a1 = m["Button1"][0]
+        b1 = m["Button2"][0]
+        c1 = m["Prize"][0] + delta
+        a2 = m["Button1"][1]
+        b2 = m["Button2"][1]
+        c2 = m["Prize"][1] + delta
+        if a1 * b2 - b1 * a2 == 0:
+            continue
+        x = (c1 * b2 - b1 * c2) / (a1 * b2 - b1 * a2)
+        y = (a1 * c2 - c1 * a2) / (a1 * b2 - b1 * a2)
+        if x.is_integer() and y.is_integer() and (not not_more_than_100 or (x <= 100 and y <= 100)):
+            result += 3 * int(x) + int(y)
+    return result
+
+
+print("Task 1:", solve_using_cramers_rule(not_more_than_100=True, delta=0))
+print("Task 2:", solve_using_cramers_rule(not_more_than_100=False, delta=10000000000000))
