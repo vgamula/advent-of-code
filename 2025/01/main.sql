ac3a149ea6abfb51e5af72930b31c5a16280b3cb
Volodymyr Gamula <v.gamula@gmail.com>
2025-12-01 19:48:47 +0000

    2025-01

---
+++ 2025/01/main.sql
@@ -0,0 +1,50 @@
+-- duckdb -f main.sql
+with recursive
+  commands as materialized (
+    select
+      command,
+      if (command[1] = 'L', -1, 1) * (cast(substring(command, 2) as int) % 100) as ticks,
+      cast(substring(command, 2) as int) // 100 as full_rotations,
+      idx
+    from
+      -- read_text('input01.txt'),
+      read_text('example01.txt'),
+      unnest(split(content, e'\n'))
+    with
+      ordinality as t (command, idx)
+    where
+      command <> ''
+    order by
+      idx
+  ),
+  result (idx) as (
+    select
+      0 as idx,
+      50 as dial_position,
+      0 as number_of_times_dial_pointed_at_zero
+    union all
+    select
+      cmd.idx,
+      (100 + prev.dial_position + cmd.ticks) % 100 as dial_position,
+      cmd.full_rotations + if (prev.dial_position + cmd.ticks > 99, 1, 0) + if (
+        0 < prev.dial_position
+        and prev.dial_position + cmd.ticks <= 0,
+        1,
+        0
+      ) as number_of_times_dial_pointed_at_zero
+    from
+      commands cmd
+      join result prev on cmd.idx = prev.idx + 1
+    where
+      cmd.idx <= (
+        select
+          count(*) as count
+        from
+          commands
+      )
+  )
+select
+  count_if(dial_position = 0) as task1,
+  sum(number_of_times_dial_pointed_at_zero) as task2
+from
+  result
