3130faf1595fe36f8848cf4c1cf7f6b917da6f5f
Volodymyr Gamula <v.gamula@gmail.com>
2025-12-03 20:59:43 +0000

    2025-03 Compute max joltage without intermediate list

--- 2025/03/Main.hs
+++ 2025/03/Main.hs
@@ -1,31 +1,35 @@
+import Data.Char
 import Data.List
 import Data.Maybe
 import Text.Printf
 
+type Bank = [Int]
+
 slice :: [a] -> Int -> Int -> [a]
 slice xs from to = take (to - from) (drop from xs)
 
-maxJoltage :: String -> [Char] -> Int -> Int -> Int
-maxJoltage bank result since left =
+maxJoltage :: Bank -> Int -> Int -> Int -> Int
+maxJoltage bank since left result =
   if left == 0
-    then read result :: Int
+    then result
     else
       let s = slice bank since (length bank - left + 1)
           maxSoFar = maximum s
           idx = fromJust $ elemIndex maxSoFar s
-       in maxJoltage bank (result ++ [maxSoFar]) (since + idx + 1) (left - 1)
+       in maxJoltage bank (since + idx + 1) (left - 1) (result * 10 + maxSoFar)
 
-solve :: Int -> [String] -> Int
-solve expectedLength banks =
-  let poweredBanks = map (\bank -> maxJoltage bank [] 0 expectedLength) banks
+totalOutput :: Int -> [Bank] -> Int
+totalOutput cellsCount banks =
+  let poweredBanks = map (\bank -> maxJoltage bank 0 cellsCount 0) banks
    in sum poweredBanks
 
 main :: IO ()
 main = do
   content <- readFile "example01.txt"
-  let banks = lines content
-      t1 = solve 2 banks
-      t2 = solve 12 banks
+  let deserializeBank = map digitToInt
+      banks = map deserializeBank $ lines content
+      t1 = totalOutput 2 banks
+      t2 = totalOutput 12 banks
    in do
         printf "Task 1: %d\n" t1
         printf "Task 2: %d\n" t2
